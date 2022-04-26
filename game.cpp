f7fc5dcfc8ca5a18291dd737474a7c8561c51002
all-other-usernames-were-taken <74026992+all-other-usernames-were-taken@users.noreply.github.com>
2022-04-26 19:42:39 -0400

    .

--- game.cpp
+++ game.cpp
@@ -5,7 +5,8 @@
 
 #define MAX_BEAMS	100
 #define PLAYER_RADIUS	8
-#define BEAM_THICKNESS	64
+#define BEAM_MAXTHICKNESS	16
+#define BEAM_MINTHICKNESS	8
 
 #define FONTPATH	"/usr/share/fonts/truetype/liberation/LiberationSans-Bold.ttf"
 #define FONTSZ	24
@@ -22,6 +23,7 @@
 		exit (1); \
 	}
 #define rnd(range)	((rand () / float (RAND_MAX)) * range)
+#define rndf	(rand () / float (RAND_MAX))
 
 #define MASK	0x00FF0000, 0x0000FF00, 0x000000FF, 0xFF000000
 
@@ -60,20 +62,6 @@ struct Bkg {
 	SDL_Surface *surface;
 } Bkg;
 
-struct Score {
-	SDL_Surface *surface;
-	long current = 0;
-	long maximum = 0;
-	bool changed = true;
-} Score;
-
-
-struct CurLevel {
-	CCoord beamvel = { .001, 1 };
-	size_t beamcnt = 1;
-	long nextlv = 0;
-} CurLevel;
-
 void ProcessEvent (SDL_JoyAxisEvent axis) {
 	switch (axis.axis) {
 		case 0: // X
@@ -84,6 +72,33 @@ void ProcessEvent (SDL_JoyAxisEvent axis) {
 			break;
 	}
 }
+
+
+namespace Score {
+	SDL_Surface *surface;
+	long current = 0;
+	long long total = 0;
+	long maximum = 0;
+	bool changed = true;
+	
+	void Inc (unsigned int amt) {
+		current += amt;
+		total += amt;
+		if (current > maximum)
+			maximum = current;
+		
+		changed = true;
+	}
+	
+	void Dec (unsigned int amt) {
+		current -= amt;
+		changed = true;
+	}
+	
+	void Reset (void) {
+		total = maximum = current = 0;
+	}
+};
 
 
 namespace SDL {
@@ -121,8 +136,8 @@ namespace SDL {
 	
 	void render_score (void) {
 		char str [64];
-		snprintf (str, 64, "%li / %li", Score.current, Score.maximum);
-		Score.surface = TTF_RenderText_Solid (Font, str, { 0, 0, 0, 0 });
+		snprintf (str, 64, "%li / %lli", Score::current, Score::total);
+		Score::surface = TTF_RenderText_Solid (Font, str, { 0, 0, 0, 0 });
 	}
 	
 	void wait_frame (void) {
@@ -162,12 +177,12 @@ namespace SDL {
 		
 		frame_done ();
 		
-		if (Score.changed) {
+		if (Score::changed) {
 			render_score ();
-			Score.changed = false;
+			Score::changed = false;
 		}
 		SDL_BlitSurface (Bkg.surface, NULL, Screen, NULL);
-		SDL_BlitSurface (Score.surface, NULL, Screen, NULL);
+		SDL_BlitSurface (Score::surface, NULL, Screen, NULL);
 	}
 	
 	void Init (void) {
@@ -216,24 +231,24 @@ void PlaceSprite (Sprite *sprite) {
 
 
 class Beam {
-	private:
+	public:
 		CCoord vel;
 		unsigned int breaktime;
+		unsigned short diameter;
 		Sprite sprite;
 
-	public:
 		void reinit (void) {
-			vel.d = (rnd (1) - 0.5) / 100.0;
-			vel.r = rnd (1) + .5;
+			vel.d = (rnd (1) - 0.5) / 1000.0;
+			vel.r = rnd (1) + 1;
 			
-			sprite.cpos.d = rnd (RAD_MAX);
+			sprite.cpos.d = Player.s.cpos.d + ((rndf - .5) * (RAD_MAX / 5));// * (rndf - .5))) / 2;
 			sprite.cpos.r = 0;
-			breaktime = rnd (10000);
 		}
 		
 		void Draw (void) {
 			if (breaktime) {
-				breaktime --;
+				if (-- breaktime == 0)
+					reinit ();
 				return;
 			}
 			
@@ -241,64 +256,85 @@ class Beam {
 			sprite.cpos.r += vel.r;
 			PlaceSprite (&sprite);
 			
-			if (sprite.cpos.r >= CIRC_RADIUS_MAX) {
-				Score.current += vel.r * 100;
-				Score.changed = true;
-				reinit ();
+			if (sprite.cpos.r >= SCR_WIDTH / 2) {
+				Score::Inc (vel.r * 10);
+				Restart ();
 			}
 		}
 		
 		bool Collision (void) {
-			return sqrt (pow (sprite.pos.x - Player.s.pos.x, 2) + pow (sprite.pos.y - Player.s.pos.y, 2)) < PLAYER_RADIUS;
+			if (breaktime)
+				return false;
+			else
+				return sqrt (pow (sprite.pos.x - Player.s.pos.x, 2) + pow (sprite.pos.y - Player.s.pos.y, 2)) < diameter;
 		}
 		
+		void Restart (void) { breaktime = rnd (128); }
+		
 		Beam (void) {
-			SDL_FreeSurface (sprite.surface);
+			diameter = (rndf * BEAM_MAXTHICKNESS) + BEAM_MINTHICKNESS;
 			
 			sprite.surface = SDL_CreateRGBSurface (
 				SDL_SWSURFACE,
-				BEAM_THICKNESS, BEAM_THICKNESS,
+				diameter, diameter,
 				32, MASK
 			);
 			
 			SDL_FillRect (sprite.surface, NULL, 0xFF000000 | rand ());
-			reinit ();
+			Restart ();
+			//reinit ();
+		}
+		
+		~Beam (void) {
+			SDL_FreeSurface (sprite.surface);
 		}
 };
 
 
-
-int main (void) {
-	Beam *beam = new Beam [MAX_BEAMS];
-	//size_t beams = 0;
-	SDL::Init ();
-	
-	Player.s.cpos = { 0, CIRC_RADIUS_DEF };
-	Player.vel = { 0, 0 };
-	while (true) {
-		Player.s.cpos.r += Player.vel.y;
-		Player.s.cpos.d += Player.vel.x;
+namespace Game {
+	long Run (void) {
+		Beam *beam [MAX_BEAMS];
+		size_t beams = 1;
+		long long nextbeamat = 10;
 		
-		if (Player.s.cpos.r > CIRC_RADIUS_MAX)
-			Player.s.cpos.r = CIRC_RADIUS_MAX;
-		else
-		if (Player.s.cpos.r < CIRC_RADIUS_MIN)
-			Player.s.cpos.r = CIRC_RADIUS_MIN;
+		Player.s.cpos = { 0, CIRC_RADIUS_DEF };
+		Player.vel = { 0, 0 };
 		
-		PlaceSprite (&Player.s);
-		
-		
-		for (size_t i = 0; i != 100; i ++) {
-			beam [i].Draw ();
+		while (Score::current >= 0) {
+			Player.s.cpos.r += Player.vel.y;
+			Player.s.cpos.d += Player.vel.x;
 			
-			if (beam [i].Collision ()) {
-				Score.current -= 10; //(Score.current / 16) - 10;
-				Score.changed = true;
-				beam [i].reinit ();
+			if (Player.s.cpos.r > CIRC_RADIUS_MAX)
+				Player.s.cpos.r = CIRC_RADIUS_MAX;
+			else
+			if (Player.s.cpos.r < CIRC_RADIUS_MIN)
+				Player.s.cpos.r = CIRC_RADIUS_MIN;
+			
+			PlaceSprite (&Player.s);
+			
+			
+			for (size_t i = 0; i != beams; i ++) {
+				beam [i].Draw ();
+				
+				if (beam [i].Collision ()) {
+					Score::Dec (((Score::total / 16) + 10) * (beam [i].diameter / BEAM_MAXTHICKNESS));
+					beam [i].Restart ();
+				}
 			}
+			
+			if (Score::total > nextbeamat) {
+				nextbeamat += Score::total;
+				if (beams < MAX_BEAMS)
+					beams ++;
+			}
+			
+			SDL::Frame ();
 		}
 		
-		
-		SDL::Frame ();
+		return Score::maximum;
 	}
+}
+		
+int main (void) {
+	SDL::Init ();
 }
